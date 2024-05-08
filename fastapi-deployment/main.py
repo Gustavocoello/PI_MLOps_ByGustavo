2d23e32fd1e410476fac14b48d728d024f20a37b
Gustavocoello <coellog634@gmail.com>
2024-05-08 00:09:47 -0500

    Archivo

--- fastapi-deployment/main.py
+++ fastapi-deployment/main.py
@@ -3,19 +3,12 @@ from pydantic import BaseModel
 from typing import Optional
 import pandas as pd
 import os
-
+import uvicorn
 # # proyecth-env\scripts\activate.bat (Activar el entorno virtual)
 # uvicorn main:app --reload (Activar fastApi)
 app = FastAPI()
    
-
-# Obtener la ruta del directorio actual
-current_dir = os.path.dirname(__file__)
-# Ruta completa del archivo
-file_path = os.path.join(current_dir, "Playtime.parquet")
-# Lectura de datos 
-df_games_items = pd.read_parquet(file_path)
-
+df_games_items = pd.read_parquet('Playtime.parquet')
 
 @app.get("/")
 def index():
@@ -23,7 +16,9 @@ def index():
 
 @app.get("/PlayTimeGenre/{genero}")
 async def PlaytimeGenre(genero: str):
-    filtro = df_games_items[df_games_items['genres'] == genero]
-    group = filtro[filtro['playtime_forever'] == filtro['playtime_forever'].max()]
-    max_year = group['Year'].values[0]
-    return {"Año de lanzamiento con más horas jugadas para Género " + genero: max_year}
+    result = df_games_items[df_games_items["genres"] == genero]
+
+    # Encontrar el año con más horas jugadas para el género
+    año_con_mas_horas = list(df_games_items[df_games_items["playtime"] == df_games_items["playtime"].max()]["release_year"])[0]
+    return {f"Año de lanzamiento con más horas jugadas para {genero}": año_con_mas_horas}
+
